c8bbd1bbc20e46d54c897d4acba374ed746be2ff
MohdSaquib114 <msmsaq114@gmail.com>
2025-06-30 17:07:44 +0530

    update port

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ async fn main() {
     let app = Router::new().route("/", get(root_handler));
 
     // listen on localhost:3000
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     println!("🚀 Server running at http://{}", addr);
 
     axum::Server::bind(&addr)
